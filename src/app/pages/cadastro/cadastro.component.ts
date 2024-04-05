9b7a163569eb5caee2700f0bdf0e7576919ed04f
Randy Gomes <raandy.gc@gmail.com>
2024-04-05 19:03:12 -0300

    Comentários do código adicionados.

--- src/app/pages/cadastro/cadastro.component.ts
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -13,11 +13,14 @@ import { Router } from '@angular/router';
   styleUrl: './cadastro.component.scss'
 })
 export class CadastroComponent {
+
+  // Criação de títulos dinâmicos
   titleForm: string = "Cadastre-se"
   titleMain: string = "Sua jornada saudável começa aqui!"
 
   constructor(private router: Router){}
 
+  // Criação Model FormGroup
   cadastroUsuario = new FormGroup({
     nome: new FormControl(''),
     dataNascimento: new FormControl(''),
@@ -26,7 +29,11 @@ export class CadastroComponent {
     confirmarSenha: new FormControl(''),
   })
 
-  
+  // Função cadastrar() utilizando o ReactiveForms, esta função está atrelada ao botão type=Submit e captura os dados inseridos no HTML.
+  // Primeiramente ela faz uma verificação se a Senha e ConfirmarSenha estão iguais.
+  // Se as senhas estiverem iguais irá salvar os dados capturados em um objeto na base de dados (LocalStorage),
+  // após será exibido um alert informando que o usuário foi cadastrado com sucesso e será redirecionado para a página /login
+  // Se as senhas não forem iguais, será exibido um alert informando que as senhas não conferem
 
   cadastrar(){   
     if (this.cadastroUsuario.value.senha === this.cadastroUsuario.value.confirmarSenha) {
@@ -54,6 +61,7 @@ export class CadastroComponent {
     }
   };
 
+  // Função fazerLogin() redireciona o usuário para a página de login.
   fazerLogin(){
     this.router.navigateByUrl('/login')
   }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -12,9 +12,10 @@ import { Router } from '@angular/router';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+  // Criação do LocalStorage
   usuariosCadastrados: any[] = [];
 
+  // Criação Model object
   loginObj: any = {
     email: "",
     senha: ""
@@ -22,13 +23,18 @@ export class LoginComponent {
 
   constructor(private router: Router) { };
 
-  ngOnInit() {
+  // Utilizado a função ngOninit() para chamar o localStorage assim que a página for carregada.
+    ngOnInit() {
     const localData = localStorage.getItem('usuariosCadastrados');
     if (localData != null) {
       this.usuariosCadastrados = JSON.parse(localData)
     }
   };
 
+  // A função Entrar() está atrelada ao botão type=Submit no HTML, 
+  // Ela faz uma verificação se o usuário já possui cadastro na base de dados (LocalStorage), fazendo uma busca pelo Email e Senha cadastrados.
+  // Se ao digitar as informações e o usuário já estiver cadastrado, será redirecionado para a página /home.
+  // Se não digitar os dados corretos, irá receber a mensagem "Dados de login incorretos"
   entrar() {
     const usuarioCadastrado = this.usuariosCadastrados.find(m => m.email == this.loginObj.email && m.senha == this.loginObj.senha);
     if (usuarioCadastrado != undefined) {
@@ -39,13 +45,18 @@ export class LoginComponent {
     }
   };
 
- resetSenha() {    
+  // A função ResetSenha(), está atrelada ao link "Esqueceu sua senha?" no HTML,
+  // Ao clicar no link irá abrir um Prompt solicitando que o usuário digite o seu email cadastrado.
+  // Será feita uma verificação se o email digitado está cadastrado na base de dados (LocalStorage),
+  // Se o usuário for localizado sua senha será modificada para o padrão "a1b2c4d4" e receberá um alert informando que a senha foi redefinida.
+  // Se o usuário não estiver cadastrado, receberá um alert informando que o usuário não foi localizado com o email informado.
+  resetSenha() {
     const email = prompt('Digite o seu email:');
 
-    if (email) {            
+    if (email) {
       const usuarioEncontrado = this.usuariosCadastrados.find((usuario: any) => usuario.email === email);
 
-      if (usuarioEncontrado) {        
+      if (usuarioEncontrado) {
         usuarioEncontrado.senha = 'a1b2c4d4';
         localStorage.setItem('usuariosCadastrados', JSON.stringify(this.usuariosCadastrados));
         alert('Senha redefinida com sucesso! Nova senha: a1b2c4d4');
@@ -57,6 +68,7 @@ export class LoginComponent {
     }
   }
 
+  // Função cadastrar() redireciona o usuário para a página de cadastro.
   cadastrar() {
     this.router.navigateByUrl('/cadastro')
   };
